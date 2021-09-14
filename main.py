3c5def928c10ee5c70815ea0e39a91d7992d0137
Andrea Mancini <mancioman2@gmail.com>
2021-09-14 15:57:28 +0200

    first push
    first push
    
    Delete .idea directory

---
+++ main.py
@@ -0,0 +1,36 @@
+import qiskit as qk
+
+
+def main():
+
+    # Creating Qubits
+    q = qk.QuantumRegister(2)
+    # Creating Classical Bits
+    c = qk.ClassicalRegister(2)
+
+    circuit = qk.QuantumCircuit(q, c)
+
+    # Hadamard Gate on the first Qubit
+    circuit.h(q[0])
+    circuit.h(q[1])
+    # CNOT Gate on the first and second Qubits
+    circuit.cx(q[0], q[1])
+    # Measuring the Qubits
+    circuit.measure(q, c)
+
+    print(circuit)
+
+    # Using Qiskit Aer's Qasm Simulator
+    simulator = qk.BasicAer.get_backend('qasm_simulator')
+
+    # Simulating the circuit using the simulator to get the result
+    job = qk.execute(circuit, simulator)
+    result = job.result()
+
+    # Getting the aggregated binary outcomes of the circuit.
+    counts = result.get_counts(circuit)
+    print(counts)
+
+
+if __name__ == '__main__':
+    main()
